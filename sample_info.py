caee3717b6394991b68dc635f4590dac1e37fa7e
Hellodeveloperls <6sonu6monu6@gmail.com>
2025-04-18 16:22:12 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -19,11 +19,9 @@ ADMINS = [12345789, 'admin123', 98765432]
 CHANNELS = [-10012345678, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
-AUTH_CHANNEL2 = None
 
 # MongoDB information
 DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
-
 
