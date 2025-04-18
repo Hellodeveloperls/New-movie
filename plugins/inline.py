1f48f7b77b20f4b899169ad257c6e70e83508682
Hellodeveloperls <6sonu6monu6@gmail.com>
2025-04-18 15:19:45 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,7 +4,7 @@ from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, AUTH_CHANNEL2 CUSTOM_FILE_CAPTION
 from database.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
@@ -40,6 +40,16 @@ async def answer(bot, query):
         return
 
     results = []
+
+    if AUTH_CHANNEL2 and not await is_subscribed2(bot, query):
+        await query.answer(results=[],
+                           cache_time=0,
+                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_parameter="subscribe")
+        return
+
+    results = []
+
     if '|' in query.query:
         string, file_type = query.query.split('|', maxsplit=1)
         string = string.strip()
